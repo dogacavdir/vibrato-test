90170d178b3675db66613e4afd9f1cc5b253ce01
dogacavdir <doga_cavdir@yahoo.com>
2016-07-07 09:35:02 +0200

    Change to calculate pitch salience function frame by frame

--- Pitch_Contour.py
+++ Pitch_Contour.py
@@ -1,10 +1,10 @@
-from essentia.standard import *
-from pylab import *
-from numpy import *
+from essentia.standard import MonoLoader,EqualLoudness,Windowing,Spectrum,SpectralPeaks,PitchSalienceFunction,PitchSalienceFunctionPeaks,PitchContours,FrameGenerator
+import pylab as plt
 from matplotlib.pylab import *
+#plot,subplot,title,xlabel,ylabel,show,grid,figure
 import numpy as np
 
-loader = MonoLoader(filename = 'Neumman-32 render 001.wav')
+loader = MonoLoader(filename = 'Neumman render 002.wav')
 audio = loader()
 
 frame_vector = []
@@ -13,8 +13,6 @@ loudness_vector = []
 pitch_confidence_vector = []
 spectrum_vector = []
 spectral_peak_vector = []
-frequencies = []
-magnitudes = []
 
 hopSize = 128
 frameSize = 2048
@@ -33,7 +31,7 @@ spectrum = Spectrum(size=size)
 spectral_peaks = SpectralPeaks(sampleRate=sampleRate,orderBy='frequency')
 salience_function = PitchSalienceFunction(binResolution=binResolution)
 salience_function_peaks = PitchSalienceFunctionPeaks(binResolution=binResolution)
-
+pitch_contours = PitchContours(binResolution=binResolution,hopSize=hopSize,sampleRate=sampleRate)
 
 filtered_signal = equal_loudness(audio)
 for frame in FrameGenerator(filtered_signal,frameSize,hopSize):
@@ -41,15 +39,62 @@ for frame in FrameGenerator(filtered_signal,frameSize,hopSize):
     spec = spectrum(w(frame))
     spectrum_vector.append(spec)
 
+frequencies_vector = []
+magnitudes_vector = []
+salienceFunction = []
+function_vector = []
+
 for i in range(len(spectrum_vector)):
     frequencies,magnitudes = spectral_peaks(spectrum_vector[i])
+    frequencies_vector.append(frequencies)
+    magnitudes_vector.append(magnitudes)
 
-salienceFunction = salience_function(frequencies,magnitudes)
-salienceBins,salienceValues = salience_function_peaks(salienceFunction)
+#salienceFunction=salience_function(frequencies_vector[0],magnitudes_vector[0])
+#salienceFunction1=salience_function(frequencies_vector[1],magnitudes_vector[1])
+#salienceFunction[3183]=salience_function(frequencies_vector[3183],magnitudes_vector[3183])
 
 
+for i in range(len(spectrum_vector)):
+    salienceFunction = salience_function(frequencies_vector[i],magnitudes_vector[i])
+    function_vector.append(salienceFunction)
+
+print len(frame_vector)
 
 fig1 = plt.figure(1)
+ax1 = fig1.add_subplot(211)
+ax1.plot(salienceFunction)
+grid()
+title('Salience Function')
+
+fig2 = plt.figure(1)
+ax1 = fig2.add_subplot(212)
+ax1.plot(frequencies_vector[0],magnitudes_vector[0])
+grid()
+title('Frequencies of the first frame vs Magnitudes')
+show()
+
+
+peakBins = []
+peakSaliences = []
+salienceBins = []
+salienceValues = []
+
+for i in range(len(frame_vector)):
+    salienceBins,salienceValues = salience_function_peaks(function_vector[i])
+    peakBins.append(salienceBins)
+    peakSaliences.append(salienceValues)
+
+contoursBins = []
+contoursSaliences = []
+contoursStartTimes = []
+duration = 0
+
+
+contoursBins,contoursSaliences,contoursStartTimes,duration = pitch_contours(peakBins,peakSaliences)
+
+print contoursBins,contoursSaliences,contoursStartTimes,duration
+
+'''fig1 = plt.figure(1)
 ax1 = fig1.add_subplot(211)
 ax1.plot(salienceBins)
 grid()
@@ -60,5 +105,5 @@ ax2 = fig2.add_subplot(212)
 ax2.plot(salienceValues)
 grid()
 title('Salience Values')
-show()
+show()'''
 
