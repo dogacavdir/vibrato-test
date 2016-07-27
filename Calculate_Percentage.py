8ac937604d3f88c8874f6446f602b838e41f07c1
dogacavdir <doga_cavdir@yahoo.com>
2016-07-27 18:40:38 +0200

    Modification for use of json

--- Calculate_Percentage.py
+++ Calculate_Percentage.py
@@ -2,15 +2,20 @@ import numpy as np
 from numpy import *
 import pandas as pd
 from derivative_peak_percentage import derivative_peak_percentage
+import json
 
-df = pd.read_csv('Violin Recordings Annotation - Sheet1.csv')
-df_new = df[df.Type == 'note']
-df_new = df_new[28:57]
-df_new["Percentage of Derivative Peaks"] = np.nan
-for file in df_new.File_Name:
+with open('input_data.json', 'r') as data_file:
+    data = json.load(data_file)
+
+# vibrato_rate = 'slow vibrato'
+# df = pd.read_csv('Violin_Recordings_Annotation_Sheet1.csv')
+# df_new = df[df.Type == 'note']
+# df_new["Percentage_of_Derivative_Peaks"] = np.nan
+# files = df_new[df_new['Presence_of_Vibrato'] == vibrato_rate]
+percentages = []
+for i in range(len(data)):
+    file = data[i]['File_Name']
     percentage = derivative_peak_percentage(file)
-    index = df_new[df_new.File_Name == file].index
-    df_new.set_value(index,"Percentage of Derivative Peaks", percentage)
+    print(file, percentage)
+    percentages.append(percentage)
 pass
-
-print df_new
