1a39d50d7194ff165364cf004103cbb98dbae8e8
dogacavdir <doga_cavdir@yahoo.com>
2016-08-31 14:23:04 +0200

    Add derivative analysis code

--- derivative_analysis.py
+++ derivative_analysis.py
@@ -2,30 +2,15 @@ from essentia import *
 from essentia.standard import *
 from numpy import *
 import matplotlib.pyplot as plt
+import collections as cll
 import pandas as pd
-from pandas import DataFrame
-import json
-
-with open('input_data.json', 'r') as data_file:
-    data = json.load(data_file)
-
-# CSV File - Data Extraction
-#
-# df = pd.read_csv('Violin Recordings Annotation - Sheet1.csv',index_col = 'Note')
-# df_new = df[df.Type == 'note']
-# df_no_vibrato = df_new[0:28]
-# df_std = df_new[28:57]
-# df_slow = df_new[57:86]
-# df_fast = df_new[86:115]
-# dataf = df_slow
-# In this example we will extract predominant melody given an audio file by
-# running a chain of algorithms.
 
 # First, create our algorithms:
 hopSize = 128
 frameSize = 2048
 sampleRate = 44100
 guessUnvoiced = True
+binResolution = 10
 
 run_windowing = Windowing(type='hann', zeroPadding=4 * frameSize)  # Hann window with x4 zero padding
 run_spectrum = Spectrum(size=frameSize * 4)
@@ -39,22 +24,34 @@ run_pitch_salience_function = PitchSalienceFunction()
 run_pitch_salience_function_peaks = PitchSalienceFunctionPeaks()
 run_pitch_contours = PitchContours(hopSize=hopSize)
 run_pitch_contours_melody = PitchContoursMelody(guessUnvoiced=guessUnvoiced,
-                                                hopSize=hopSize)
+                                                hopSize=hopSize,
+                                                binResolution=binResolution)
 
-# ... and create a Pool
-pool = Pool()
 
-# Now we are ready to start processing.
-# 1. Load audio and pass it through the equal-loudness filter
-#Files vector will be the first column of the CSV File, File_Name
-files = dataf.File_Name
 derivative_vector = []
 pitch_vector = []
 n_frames_vector = []
 percentage_vector = []
+time_vector = []
+difference_vector = []
+mean_vector = []
+max_vector = []
+index_max_vector = []
+max_loc_vector = []
+
+df = pd.read_csv('violin_vibrato.csv')
+files = []
+# Selecting which catagory to be anaylzed
+for i in range(len(df.File_Name)):
+    if df.Note[i] == 'A4':
+        files.append(df.File_Name[i])
+
 for file in files:
     audio = MonoLoader(filename=file)()
     audio = EqualLoudness()(audio)
+
+    # ... and create a Pool
+    pool = Pool();
 
     # 2. Cut audio into frames and compute for each frame:
     #    spectrum -> spectral peaks -> pitch salience function -> pitch salience function peaks
@@ -78,17 +75,19 @@ for file in files:
                                                   contours_saliences,
                                                   contours_start_times,
                                                   duration)
+    # Eliminating the noise at the beginning and at the end
+    abs_confidence = abs(confidence)
+    m_common = cll.Counter(abs_confidence).most_common()[0]
+    first = [index for index, val in enumerate(abs_confidence) if val == m_common[0]][0]
+    last = [index for index, val in enumerate(abs_confidence) if val == m_common[0]][-1]
+    pitch[0:first + 1] = 0
+    pitch[last:] = 0
     pitch_vector.append(pitch)
-    # NOTE that we can avoid the majority of intermediate steps by using a composite algorithm
-    #      PredominantMelody (see extractor_predominant_melody.py). This script will be usefull
-    #      if you want to get access to pitch salience function and pitch contours.
 
     n_frames = len(pitch)
-    # n_frames_vector.append(n_frames)
-    # print "number of frames:", n_frames
 
-
-    time=range(n_frames-1)
+    time=arange(0,duration,duration/float(len(pitch)))
+    time_vector.append(time)
     dr1 = derivative(pitch)
     high_derivative_indexes = [index for index, val in enumerate(dr1) if val>100]
     normalized_derivatives = [val for index, val in enumerate(dr1)
@@ -98,14 +97,6 @@ for file in files:
                               if index not in low_derivative_indexes]
 
     derivative_vector.append(normalized_derivatives)
-    # dr2 = derivative(dr1)
-    # high_second_derivative_indexes = [index for index, val in enumerate(dr2) if val > 100]
-    # normalized_second_derivatives = [val for index, val in enumerate(dr2)
-    #                           if index not in high_second_derivative_indexes]
-    # low_second_derivative_indexes = [index for index, val in enumerate(normalized_second_derivatives) if val < -100]
-    # normalized_second_derivatives = [val for index, val in enumerate(normalized_second_derivatives)
-    #                           if index not in low_second_derivative_indexes]
-    # second_derivative_vector.append(normalized_second_derivatives)
 
     # Calculate the percentage of peaks od the pitch countor's derivative
     peaknumber = 0
@@ -114,11 +105,32 @@ for file in files:
     peak_percentage = peaknumber/length*100.0
     percentage_vector.append(peak_percentage)
 
+    # Calculates the differneces between peaks
+    difference = diff(normalized_derivatives)
+    difference_vector.append(difference)
+    # Calculates the mean and max values of the difference array
+    mean_vector.append(mean(difference))
+    max_vector.append(max(difference))
 
-# Need to save the value into the new percentage column of the CSV File
-dataf['Percentage_of_Derivative_Peaks'] = percentage_vector
-# df_new.to_csv('Violin_Vibrato.csv', sep=',')
+    # Calculate the index of max differences to see if they appear in the end or in the beginning
+    index_max = [loc for loc,val in enumerate(difference) if val == max(difference)]
+    index_max_vector.append(index_max)
+    for i in range(len(index_max)):
+        max_loc = (index_max[i]+1)/float(len(difference))
+        max_loc_vector.append(max_loc)
 
-max_value = max(df_new.Percentage_of_Derivative_Peaks)
-mean_value = mean(df_new.Percentage_of_Derivative_Peaks)
-min_value = min(df_new.Percentage_of_Derivative_Peaks)
+    plt.figure()
+    plt.subplot(211)
+    plt.plot(time,pitch)
+    plt.ylabel('Frequency (Hz)')
+    plt.xlabel('time (s)')
+    plt.suptitle('Pitch')
+    plt.subplot(212)
+    plt.plot(range(len(difference)),difference)
+    plt.ylabel('Peaks')
+    plt.xlabel('Frame Size')
+    plt.suptitle('First Derivative of the Pitch')
+
+plt.show()
+
+pass
